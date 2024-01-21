fe77675cea474a1497c6320827bb1835bf36ca14
Yuzuru <109451963+Yuzuru07@users.noreply.github.com>
2024-01-21 17:36:16 +0800

    menghilangkan debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
