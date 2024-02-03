48225a7767abb8b14f9e718beeb95e69b8be40c2
Yuzuru <109451963+Yuzuru07@users.noreply.github.com>
2024-02-03 21:41:49 +0800

    Menambahkann fungsionalitas Logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:kontak/controllers/auth_service.dart';
 import 'package:kontak/firebase_options.dart';
 import 'package:kontak/views/home.dart';
 import 'package:kontak/views/login_page.dart';
@@ -11,29 +12,47 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    title: 'Quantum Connect',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      textTheme: GoogleFonts.soraTextTheme(),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange.shade800),
+      useMaterial3: true,
+    ),
+    routes: {
+      "/": (context) => const CheckUser(),
+      "/home": (context) => const HomePage(),
+      "/signup": (context) => const SignUpPG(),
+      "/login": (context) => const LoginPG()
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CheckUser extends StatefulWidget {
+  const CheckUser({super.key});
 
-  // This widget is the root of the application.
+  @override
+  State<CheckUser> createState() => _CheckUserState();
+}
+
+class _CheckUserState extends State<CheckUser> {
+  @override
+  void initState() {
+    AuthService().isLoggedIn().then((value) {
+      if (value) {
+        Navigator.pushReplacementNamed(context, "/home");
+      } else {
+        Navigator.pushReplacementNamed(context, "/login");
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quantum Connect',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.soraTextTheme(),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange.shade800),
-        useMaterial3: true,
-      ),
-      routes: {
-        "/": (context) => const LoginPG(),
-        "/home": (context) => const HomePage(),
-        "/signup": (context) => const SignUpPG(),
-        "/login": (context) => const LoginPG()
-      },
+    return const Center(
+      child: CircularProgressIndicator(),
     );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kontak/controllers/auth_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +14,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Quantum Connect")),
-      body: const Text("Intinya sdh login"),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 33,
+                  child: Text(FirebaseAuth.instance.currentUser!.email
+                      .toString()[0]
+                      .toUpperCase()),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(FirebaseAuth.instance.currentUser!.email.toString())
+              ],
+            )),
+            ListTile(
+              onTap: () {
+                AuthService().logout();
+                Navigator.pushReplacementNamed(context, "/login");
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Logout berhasil")));
+              },
+              leading: const Icon(Icons.logout_outlined),
+              title: const Text("Logout"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
